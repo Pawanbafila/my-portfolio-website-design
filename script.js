6248d1fedb79c997a252623a8ac16e79835fa085
Pawan Bafila <123593267+Pawanbafila@users.noreply.github.com>
2024-03-14 00:41:34 +0530

    add some animation

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-gsap.from( ".banner-text-img .banner-img1",{
-scale:1,
-duration: 1,
-opacity:0,
-x:200,
-scrub:5,
+// gsap.from( ".banner-text-img .banner-img1",{
+// scale:1,
+// duration: 1,
+// opacity:0,
+// x:200,
+// scrub:5,
 
-})
-gsap.from( ".banner-p",{
-x:-1000,
-duration: 1,
-opacity:0,
-scrub:5,
+// })
+// gsap.from( ".banner-p",{
+// x:-1000,
+// duration: 1,
+// opacity:0,
+// scrub:5,
 
-})
-gsap.from( ".banner-h",{
+// })
+// gsap.from( ".banner-h",{
 
-duration: 1,
-opacity:0,
-scale:2,
-scrub:5,
-})
+// duration: 1,
+// opacity:0,
+// scale:2,
+// scrub:5,
+// })
 
 
-gsap.from ( ".about-me p",{
-    scale:1,
-    duration: 1,
-    opacity:0,
-    x:-200,
+// gsap.from ( ".about-me p",{
+//     scale:1,
+//     duration: 1,
+//     opacity:0,
+//     x:-200,
     
-    scrollTrigger: ".about-us p"
-    })
+//     scrollTrigger: ".about-us p"
+//     })
